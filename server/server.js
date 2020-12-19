90ec09f858513ddb14553eefd7a5fd7e961765ee
evperlman <evperlman@gmail.com>
2020-12-19 11:06:13 -0800

    test commit on branch

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { exitCode } = require('process');
 
 
+//routers:
+const apiRouter = require('./routes/api.js');
 
 
+//route handlers:
+app.use('/test1')
 
-
-
-
-
-
-
+// a new test.
+//lets check out if this works. 
 
 // statically serve everything in the build folder on the route '/build'
 app.use('/build', express.static(path.join(__dirname, '../build')));
