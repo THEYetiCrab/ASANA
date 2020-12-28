a1c55623fa72098133b96412c57c2a9a1caa7d07
kitloong <kitloong@gmail.com>
2020-12-27 19:37:49 -0600

    commit before merge

--- client/components/Account.jsx
+++ client/components/Account.jsx
@@ -3,13 +3,27 @@ import React, {useState, useEffect} from 'react';
 
 export default function Account() {
 
-  const [account, setAccount] = useState([])
+  const [accounts, setAccounts] = useState([1, 2, 3]);
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .then(data => setAccounts(data))
+  }, [])
 
+  console.log(accounts)
+  
+  
   return (
-    <ul>
-      <li>Chase</li>
-      <li>Bank of America</li>
-  </ul>
+    <div>
+      <ul>
+        {accounts.map(account => {
+          
+          return <li key={account.id}>{account.name}</li>
+          
+        })}
+      </ul>
+    </div>
   )
 }
--- client/components/Transactions.jsx
+++ client/components/Transactions.jsx
@@ -4,14 +4,72 @@ import ScrollToBottom from 'react-scroll-to-bottom';
 import { DataGrid } from '@material-ui/data-grid';
 
 
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'merchant_name', headerName: 'Transaction', width: 130 },
+  { field: 'category', headerName: 'Category', width: 130 },
+  { field: 'date_of_transaction', headerName: 'Date', width: 150 },
+  { field: 'amount', headerName: 'Amount', type: 'number', width: 90 }
+];
 
-const Transactions = ({transactions}) => (
-  <ScrollToBottom className="transactions">
-    <h3>Transactions</h3>
-    {/* {transactions.map((transaction, i) => <div key={i}><Transaction name={transaction.name} category={transaction.category} date={transaction.date} amount={transactions.amount}/></div>)} */}
-  </ScrollToBottom>
-)
+const rows = [
+  {
+      id: 1,
+      merchant_name: "ASANA",
+      category: "Entertainment",
+      date_of_transaction: "12-19-2020",
+      amount: 100,
+  },
+  {
+      id: 2,
+      merchant_name: "ASANA",
+      amount: 100,
+      account_type: "Credit",
+      date_of_transaction: "12-19-2020",
+      category: "Entertainment"
+  },
+  {
+      id: 3,
+      account_id: "id26",
+      merchant_name: "ASANA1",
+      amount: 102,
+      account_type: "Debit",
+      date_of_transaction: "12-19-2020",
+      category: "Entertainment"
+  },
+  {
+      id: 4,
+      account_id: "id26",
+      merchant_name: "ASANA3",
+      amount: 10,
+      account_type: "Credit",
+      date_of_transaction: "12-20-2020",
+      category: "Entertainment"
+  }
+];
 
 
 
-export default Transactions;
+
+// const Transactions = ({transactions}) => (
+//   <ScrollToBottom className="transactions">
+//     <h3>Transactions</h3>
+//     {/* {transactions.map((transaction, i) => <div key={i}><Transaction name={transaction.name} category={transaction.category} date={transaction.date} amount={transactions.amount}/></div>)} */}
+//   </ScrollToBottom>
+// )
+
+
+export default function Transactions() {
+  return (
+    <ScrollToBottom className="transactions">
+      <h3>Transactions</h3>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid rows={rows} columns={columns} pageSize={6}/>
+      </div>
+    </ScrollToBottom>
+  );
+}
+
+
+
+// export default Transactions;
